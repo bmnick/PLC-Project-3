18ff46dbabba0e82128ae3aa3947a28331a19971
Ben Nicholas <ben@ben-nicholas.com>
2011-10-28 15:44:18 -0400

    problem 1 solved

---
+++ problem1.php
@@ -0,0 +1,16 @@
+<?php 
+  do {
+    echo "Input list of 5 numbers, seperated by \", \"> ";
+    $handle = fopen("php://stdin", r);
+    $line = trim(fgets($handle));
+    
+    $entries = split(", ", $line);
+  } while (count($entries) != 5);
+  
+  
+  natsort($entries);
+  
+  echo "Ascending order: " . implode(" ", $entries) . "\n";
+  echo "Descending order: " . implode(" ", array_reverse($entries)) . "\n";
+  
+?>
